de7f394055afce6cd17918afdecb4e6f8c2130f2
Elise Senechal <35364291+furelisegs@users.noreply.github.com>
2023-02-18 20:12:50 -0600

    initial commit

---
+++ HelloGoodbye.java
@@ -0,0 +1,11 @@
+package firstpackage;
+
+class HelloGoodbye {
+	 //main method definition
+	 public static void main(String args[]) {
+		 String a = args[0];
+		 String b = args[1];
+		 System.out.println("Hellow " + a + " and " + b);
+		 System.out.println("Goodbye " + b + " and " + a);
+	 }
+}
